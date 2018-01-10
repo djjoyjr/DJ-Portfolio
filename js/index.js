a7dfbc44b67c6c39fe1911b66173c1fb5c83c541
Donald Joy <djsmacbookpro-2017@Donalds-MBP.fios-router.home>
2018-01-09 22:04:51 -0500

    final version

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider').slider({
+    height: 420,
+    transition: 1000,
+    interval: 5100
+  });
+});
+
+// $(document).ready(function(){
+//   $('.parallax').parallax();
+// });
